ab4af59524ee318182972cacbb54bde06b54b5a3
Ahmed ElSawaf <ahmed.elsawaf@zalando.de>
2020-06-12 00:40:47 +0200

    feat: add cv to public profile

---
+++ cv/precache-manifest.863e6e02dbdf1b1cfeb9929fa7ac2998.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9757e4970157be626ee16a61efd2a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db1f86d7ed3e41908277",
+    "url": "/static/js/2.82a0fc8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82a0fc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153cabf34172f98a8180",
+    "url": "/static/js/main.6e8badf1.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
